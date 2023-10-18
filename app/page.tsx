5ccfd2621429a85fbcad9c29c5e60cadbb7707da
NaveenBommisetty <147597890+NaveenBommisetty@users.noreply.github.com>
2023-10-18 18:21:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Dashboard | React JS Dashboard Template",
-  description: "This is Home Blog page for Dashboard",
+  title: "React JS Dashboard ",
+  description: "Dashboard",
   // other metadata
 };
 

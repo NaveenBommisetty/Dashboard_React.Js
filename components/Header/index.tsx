3ec595eb63b4d94be2ab1fb4ddf02cf35426fde3
NaveenBommisetty <147597890+NaveenBommisetty@users.noreply.github.com>
2023-10-18 18:34:33 +0100

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 // import DarkModeSwitcher from "./DarkModeSwitcher";
 // import DropdownMessage from "./DropdownMessage";
 // import DropdownNotification from "./DropdownNotification";
-// import DropdownUser from "./DropdownUser";
+import DropdownUser from "./DropdownUser";
 import Image from "next/image";
 
 const Header = (props: {

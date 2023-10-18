21d3ce0b8e34958d337a1fdac272239839a775bf
NaveenBommisetty <147597890+NaveenBommisetty@users.noreply.github.com>
2023-10-18 18:28:42 +0100

    Chart issues fixed

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -12,14 +12,14 @@ export const metadata: Metadata = {
 const Chart = () => {
   return (
     <>
-      <Breadcrumb pageName="Chart" />
+      {/* <Breadcrumb pageName="Chart" /> */}
 
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <div className="col-span-12">
-          <ChartFour />
+          {/* <ChartFour /> */}
         </div>
         <ChartOne />
-        <ChartTwo />
+        {/* <ChartTwo /> */}
         <ChartThree />
       </div>
     </>
